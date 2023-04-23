2a1b2fdb95b363c510a7fa09091b25baa20fc7df
deimer.ballesteros <deimer.ballesteros@appgate.com>
2023-04-22 22:32:24 -0500

    Se modifican variables de entorno

--- Proyecto/Entrega_2/apis/inference/src/configuration/env.py
+++ Proyecto/Entrega_2/apis/inference/src/configuration/env.py
@@ -1,14 +1,14 @@
 import os
 
 # MODEL PATH
-MODEL_PATH = os.getenv("MODEL_PATH", "../fs/")
+MODEL_PATH = os.getenv("MODEL_PATH", "/opt/topicsIA/model/")
 # DATASET PATH
-DATASET_PATH = os.getenv("DATASET_PATH", "../fs/")
+DATASET_PATH = os.getenv("DATASET_PATH", "/opt/topicsIA/dataset/")
 # USER DB
 USER_DB = os.getenv("USER_DB", "root")
 # PASS DB
-PASS_DB = os.getenv("PASS_DB", "Deyka2023")
+PASS_DB = os.getenv("PASS_DB", "topicosIA")
 # IP SERVER DB
-IP_SERVER = os.getenv("IP_SERVER", "localhost:3306")
+IP_SERVER = os.getenv("IP_SERVER", "10.43.102.112")
 # NAME DB
-NAME_DB = os.getenv("NAME_DB", "mlops-inference")
+NAME_DB = os.getenv("NAME_DB", "inference")
--- Proyecto/Entrega_2/apis/process_data/src/configuration/env.py
+++ Proyecto/Entrega_2/apis/process_data/src/configuration/env.py
@@ -2,14 +2,14 @@ import os
 
 
 # DATASET PATH
-DATASET_PATH = os.getenv("DATASET_PATH", "../fs")
+DATASET_PATH = os.getenv("DATASET_PATH", "/opt/topicsIA/dataset")
 # DATASET CSV
-DATASET_CSV = os.getenv("DATASET_PATH", "../fs/covertype.train.csv")
+DATASET_CSV = os.getenv("DATASET_PATH", "/opt/topicsIA/dataset/covertype.train.csv")
 # USER DB
 USER_DB = os.getenv("USER_DB", "root")
 # PASS DB
-PASS_DB = os.getenv("PASS_DB", "Deyka2023")
+PASS_DB = os.getenv("PASS_DB", "topicosIA")
 # IP SERVER DB
-IP_SERVER = os.getenv("IP_SERVER", "localhost:3306")
+IP_SERVER = os.getenv("IP_SERVER", "10.43.102.112")
 # NAME DB
-NAME_DB = os.getenv("NAME_DB", "mlops-training")
+NAME_DB = os.getenv("NAME_DB", "training")
